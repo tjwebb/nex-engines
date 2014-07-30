c88833aa17fa090ef69e4530384022bfa199d4b5
Travis Webb <me@traviswebb.com>
2014-07-30 11:34:55 -0400

    use node-latest-version directly

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ var nex = require('nex-api');
 var n = require('n-api');
 var log = require('npmlog');
 var colors = require('colors');
+var resolver = require('node-latest-version');
 
 var handler = module.exports = new nex.Handler('engines');
 
 /**
  * @override
  */
-handler.do = function (pkg, resolver) {
+handler.do = function (pkg) {
   var node;
   if (pkg.engines && pkg.engines.node) {
-    node = resolver.satisfy(pkg.engines.node);
+    node = resolver.satisfy.sync(pkg.engines.node);
   }
   else {
     log.warn('package.json engines', 'is not properly set but you seem to think it is');
